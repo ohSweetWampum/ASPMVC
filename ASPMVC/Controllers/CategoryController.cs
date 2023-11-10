c268c214645c1985ecd11ac6f9ef3b58d4526b0f
mtgibson8 <126990635+mtgibson8@users.noreply.github.com>
2023-11-10 17:36:31 -0600

    had to start over with new app as I deleted some stuff in the other one

---
+++ ASPMVC/Controllers/CategoryController.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using ASPMVC.Data;
+using ASPMVC.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ASPMVC.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+        public CategoryController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+        public IActionResult Index()
+        {
+            List<Category> objCategoryList = _db.myCategoryTable.ToList();
+            return View(objCategoryList);
+        }
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Create(Category obj)
+        {
+            if (obj.Name == obj.DisplayOrder.ToString())
+            {
+                ModelState.AddModelError("name", "The DisplayOrder cannot exactly match the Name.");
+            }
+            if (ModelState.IsValid)
+            {
+                _db.myCategoryTable.Add(obj);
+                _db.SaveChanges();
+                TempData["success"] = "Category created successfully";
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+
+        public IActionResult Edit(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+            Category? categoryFromDb = _db.myCategoryTable.Find(id);
+            //Category? categoryFromDb1 = _db.myCategoryTable.FirstOrDefault(u => u.Id == id);
+            //Category? categoryFromDb2 = _db.myCategoryTable.Where(u => u.Id == id).FirstOrDefault();
+
+            if (categoryFromDb == null)
+            {
+                return NotFound();
+            }
+            return View(categoryFromDb);
+        }
+        [HttpPost]
+        public IActionResult Edit(Category obj)
+        {
+            if (obj.Name == obj.DisplayOrder.ToString())
+            {
+                ModelState.AddModelError("name", "The DisplayOrder cannot exactly match the Name.");
+            }
+
+            if (ModelState.IsValid)
+            {
+                _db.myCategoryTable.Update(obj);
+                _db.SaveChanges();
+                TempData["success"] = "Category created successfully";
+                return RedirectToAction("Index");
+            }
+            return View();
+
+        }
+        public IActionResult Delete(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+            Category? categoryFromDb = _db.myCategoryTable.Find(id);
+            //Category? categoryFromDb1 = _db.Categories.FirstOrDefault(u=>u.Id==id);
+            //Category? categoryFromDb2 = _db.Categories.Where(u=>u.Id==id).FirstOrDefault();
+
+            if (categoryFromDb == null)
+            {
+                return NotFound();
+            }
+            return View(categoryFromDb);
+        }
+        [HttpPost, ActionName("Delete")]
+        public IActionResult DeletePOST(int? id)
+        {
+            Category? obj = _db.myCategoryTable.Find(id);
+            if (obj == null)
+            {
+                return NotFound();
+            }
+            _db.myCategoryTable.Remove(obj);
+            _db.SaveChanges();
+            TempData["success"] = "Category created successfully";
+            return RedirectToAction("Index");
+        }
+    }
+}
+
---
+++ ASPMVC/Data/ApplicationDbContext.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using ASPMVC.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ASPMVC.Data
+{
+
+    //DbContext is built in class from Entityframework core
+    public class ApplicationDbContext : DbContext
+    {
+        //DbSet is built in. should be of type Category(catergory class type)
+        //to migrate the table and create it  in db, run cmmd dotnet ef migrations add [nameofmigration], will also
+        //create a migrations folder,
+        //then run dotnet ef database update, to apply migration
+        public DbSet<Category> myCategoryTable { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Category>().HasData(
+                new Category { Id = 1, Name = "Action", DisplayOrder = 1 },
+                new Category { Id = 2, Name = "SciFi", DisplayOrder = 2 },
+                new Category { Id = 3, Name = "History", DisplayOrder = 3 }
+                );
+        }
+
+        //DbContextOptions is a built in class, it is of generic type (<>)    ApplicationDbContext   
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+    }
+
+}
